a88036ae2ed9f47051dac084519a1be0b263efd0
caniot <caniot.team@gmail.com>
2022-09-25 21:57:14 +0200

    add config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,6 @@
 project = 'Caniot Tool'
 copyright = '2022, caniot team'
 author = 'caniot team'
-version = '2.0'
 release = '1.0.1'
 
 # -- General configuration ---------------------------------------------------
