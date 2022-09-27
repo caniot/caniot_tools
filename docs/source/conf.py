252d3feff403a8726d4673db5192895e7ad084aa
caniot <caniot.team@gmail.com>
2022-09-27 06:48:41 +0200

    add html_static_path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-# html_static_path = ['_static']
+html_static_path = ['_static']
 html_favicon = './images/favicon.png'
 html_title = 'Caniot Tool'
 html_logo = './images/twitter_header_photo_2.png'
